402e0e200e447a4f67f27a21b6a71b9cb071b6ed
MITLabo <MIT>
2015-12-13 23:29:13 +0900

    エラーハンドリング他

--- calculate/src/main/java/study/spring/entity/CalculateData.java
+++ calculate/src/main/java/study/spring/entity/CalculateData.java
@@ -1,11 +1,16 @@
 package study.spring.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,8 +27,13 @@ import study.spring.e.CalculateOperator;
 @NoArgsConstructor
 @Entity
 @Table(name = "CALCULATE_DATA")
-public class CalculateData implements IEntity{
+public class CalculateData implements IEntity,Serializable{
 	
+	/**
+	 * 1L
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	private String id;
 
@@ -37,6 +47,12 @@ public class CalculateData implements IEntity{
 	@Column(nullable = false)
 	private int calculateArg;
 	
+	/**
+	 * 0から10までの数値を扱う。
+	 */
+    @NotNull
+    @Min(0)
+    @Max(10)
 	@Column(nullable = false)
 	private int beCalculatedArg;
 
