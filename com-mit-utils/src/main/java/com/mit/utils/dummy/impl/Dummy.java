8a6ef2d6393035887b6a0a308d602dfe5a612f7b
MITLabo <MIT>
2016-12-23 19:46:38 +0900

    initial

---
+++ com-mit-utils/src/main/java/com/mit/utils/dummy/impl/Dummy.java
@@ -0,0 +1,24 @@
+/**
+ * テスト用実装クラスとして試験的な実装を提供する。<p>
+ */
+package com.mit.utils.dummy.impl;
+
+import com.mit.utils.dummy.IDummy;
+
+/**
+ * ハローワールドを提供する。<p>
+ * @author Kappappa
+ *
+ */
+public class Dummy implements IDummy {
+
+	/* (非 Javadoc)
+	 * @see com.mit.utils.dummy.impl.IDummy#helloWorld()
+	 */
+	@Override
+	public String helloWorld(){
+		return "HelloWorld !";
+		
+	}
+	
+}
---
+++ com-mit-utils/src/main/java/com/mit/utils/dummy/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * テスト用ダミーパッケージ
+ * @author Kappappa
+ *
+ */
+package com.mit.utils.dummy;
