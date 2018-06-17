f0ea45a7c7ff6e416a5a6a6ed16245189a9d2df3
ktr.jzs161.mit <ktr.jzs161.mit@gmail.com>
2018-06-17 07:50:45 -0400

    Add lesson4.5.

---
+++ mit-javaseminar-oo-lesson4.5/src/main/java/mit/javaseminar/oo/lesson4/Player.java
@@ -0,0 +1,93 @@
+package mit.javaseminar.oo.lesson4;
+
+/**
+ * ジャンケンを行うプレイヤークラス。
+ */
+public class Player
+{
+	/** グー */
+	public static final int STONE = 0;
+
+	/** チョキ */
+	public static final int SCISSORS = 1;
+
+	/** パー */
+	public static final int PAPER = 2;
+
+	/** プレイヤーの名前 */
+	private String name;
+
+	/** プレイヤーの勝った回数 */
+	private int winCount = 0;
+
+	/** 与えられた戦略 */
+	private Tactics tactics_;
+
+	/**
+	 * プレイヤークラスのコンストラクタ。
+	 * 
+	 * @param name 名前
+	 */
+	public Player(String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * プレイヤーに戦略を渡す。
+	 * 
+	 * @param tactics 戦略
+	 */
+	void setTactics(Tactics tactics)
+	{
+		tactics_ = tactics;
+	}
+
+	/**
+	 * ジャンケンの手を出す。
+	 *
+	 * @return ジャンケンの手
+	 */
+	int showHand()
+	{
+		// 与えられた戦略を読んでジャンケンの手を決める
+		int hand = tactics_.readTactics();
+
+		// 決定した手を戻り値として返す
+		return hand;
+	}
+
+	/**
+	 * 勝敗を聞く(教える)。
+	 *
+	 * @param result true:勝ち,false:負け
+	 */
+	void notifyResult(boolean result)
+	{
+		if (true == result)
+		{
+			// 勝った場合は勝ち数を加算する
+			winCount += 1;
+		}
+	}
+
+	/**
+	 * 自分の勝った回数を答える
+	 *
+	 * @return 勝った回数
+	 */
+	int getWinCount()
+	{
+		return winCount;
+	}
+
+	/**
+	 * 自分の名前を答える。
+	 * 
+	 * @return 名前
+	 */
+	String getName()
+	{
+		return name;
+	}
+}
