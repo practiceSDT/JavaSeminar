0d311c3afa72f16730e5dbc5c3418782a3f14b1d
mit at home <mit@domain.com>
2018-08-05 11:15:34 +0900

    moved

--- InterFaceTool-SpringBootAndIntegration/src/main/java/InterFace/sbi/endpoint/Recieved.java
+++
@@ -1,9 +0,0 @@
-package InterFace.sbi.endpoint;
-
-import org.springframework.integration.annotation.ServiceActivator;
-
-public interface Recieved {
-
-	void execute(Integer id, String st) throws Exception;
-
-}
