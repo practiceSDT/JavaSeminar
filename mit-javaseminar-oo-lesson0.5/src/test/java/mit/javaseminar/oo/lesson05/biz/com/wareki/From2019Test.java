52b43a3597607a8343e736340e745ac7df010a8f
mitsuikn <mitsuikn@domain.com>
2018-08-08 20:56:46 +0900

    com

---
+++ mit-javaseminar-oo-lesson0.5/src/test/java/mit/javaseminar/oo/lesson05/biz/com/wareki/From2019Test.java
@@ -0,0 +1,29 @@
+package mit.javaseminar.oo.lesson05.biz.com.wareki;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class From2019Test extends From2019 {
+
+	@Test
+	public void testGetGengo2String() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetGengo1String() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetGengoInitial() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetGengoNen() {
+		fail("まだ実装されていません");
+	}
+
+}
